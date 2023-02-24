307bc95add2728511574a910c5983ec42bbc271c
mahfuz khan <mahfuzpust@gmail.com>
2023-02-24 23:56:26 +0600

    Country-State-City

---
+++ CRUD_Front_Controller/Controllers/CascadeController.cs
@@ -0,0 +1,42 @@
+﻿using CRUD_Front_Controller.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CRUD_Front_Controller.Controllers
+{
+    public class CascadeController : Controller
+    {
+        private readonly ApplicationDbContext context;
+        public CascadeController(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+
+        public JsonResult Country()
+        {
+            var cnt = context.Countries.ToList();
+            return new JsonResult(cnt);
+        }
+
+        public JsonResult State(int id)
+        {
+            var st = context.States.Where(u => u.Country.Id == id).ToList();
+            return new JsonResult(st);
+        }
+        public JsonResult City(int id)
+        {
+            var ct = context.Cities.Where(u => u.State.Id == id).ToList();
+            return new JsonResult(0);
+        }
+
+
+        public IActionResult CaseCadeDropdown()
+        {
+            return View();
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ CRUD_Front_Controller/Migrations/20230224174113_citiess.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CRUDFrontController.Migrations
+{
+    /// <inheritdoc />
+    public partial class citiess : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Cities_States_StateId",
+                table: "Cities");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Cities",
+                table: "Cities");
+
+            migrationBuilder.RenameTable(
+                name: "Cities",
+                newName: "Citiess");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Cities_StateId",
+                table: "Citiess",
+                newName: "IX_Citiess_StateId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Citiess",
+                table: "Citiess",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Citiess_States_StateId",
+                table: "Citiess",
+                column: "StateId",
+                principalTable: "States",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Citiess_States_StateId",
+                table: "Citiess");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Citiess",
+                table: "Citiess");
+
+            migrationBuilder.RenameTable(
+                name: "Citiess",
+                newName: "Cities");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Citiess_StateId",
+                table: "Cities",
+                newName: "IX_Cities_StateId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Cities",
+                table: "Cities",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Cities_States_StateId",
+                table: "Cities",
+                column: "StateId",
+                principalTable: "States",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
