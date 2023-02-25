d2ba0bd80d32f9849af5fa82e21a170026573a65
mahfuz khan <mahfuzpust@gmail.com>
2023-02-26 01:11:55 +0600

    Login-SignUp formation

---
+++ CRUD_Front_Controller/Controllers/Account/AccountController.cs
@@ -0,0 +1,33 @@
+﻿using CRUD_Front_Controller.Models.ViewModel;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CRUD_Front_Controller.Controllers.Account
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Login(LoginSignUpViewModel model)
+        {
+            return View();
+        }
+        public IActionResult SignUp()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult SignUp(LoginSignUpViewModel model)
+        {
+            return View();
+        }
+
+    }
+}
